1d31a58734ac7d56db953ee7ba4b63aaa509f5d9
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-11-10 20:39:31 +0700

    Update cek-tr.sh

--- xray/cek-tr.sh
+++ xray/cek-tr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Decrypted By YADDY D PHREAKER
+#!/bin/bash
 DF='\e[39m'
 Bold='\e[1m'
 Blink='\e[5m'
@@ -30,48 +32,52 @@ function con() {
     fi
 }
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#tr#' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "              TROJAN USER LOGIN            $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
-echo -e "   User"     "       Last Login"    "  Usage"   " Total IP"
-echo -e "\033[1;91m┌──────────────────────────────────────────┐\033[0m"
+data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m           CEK TROJAN ACCOUNT           \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
-echo -n > /tmp/ipvmess.txt
+echo -n > /tmp/iptrojan.txt
 data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
 jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >> /tmp/ipvmess.txt
+echo "$jum" >> /tmp/iptrojan.txt
 else
 echo "$ip" >> /tmp/other.txt
 fi
-jum2=$(cat /tmp/ipvmess.txt)
+jum2=$(cat /tmp/iptrojan.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-jum=$(cat /tmp/ipvmess.txt)
+jum=$(cat /tmp/iptrojan.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/ipvmess.txt | wc -l)
-byte=$(cat /etc/vmess/${akun})
+#iplimit=$(cat /etc/kytxz/limit/trojan/ip/${akun})
+jum2=$(cat /tmp/iptrojan.txt | wc -l)
+byte=$(cat /etc/trojan/${akun})
 lim=$(con ${byte})
-wey=$(cat /etc/limit/vmess/${akun})
+wey=$(cat /etc/limit/trojan/${akun})
 gb=$(con ${wey})
 lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-printf "  %-13s %-7s %-8s %2s\n"   "${akun}" "$lastlogin"  " ${gb}/${lim}"   "$jum2";
+echo -e " \033[1;36m┌──────────────────────────────────────┐\033[0m"
+printf "  %-13s %-7s %-8s %2s\n"   "  USERNAME : ${akun}" | lolcat
+printf "  %-13s %-7s %-8s %2s\n" "  LOGIN    : $lastlogin" | lolcat 
+printf "  %-13s %-7s %-8s %2s\n" "  LIMIT GB : ${gb}/${lim}" | lolcat  
+#printf "  %-13s %-7s %-8s %2s\n" "  LIMIT IP : $jum2/$iplimit" | lolcat;
+echo -e " \033[1;36m└──────────────────────────────────────┘\033[0m"
 fi 
-rm -rf /tmp/ipvmess.txt
+rm -rf /tmp/iptrojan.txt
 done
 rm -rf /tmp/other.txt
 echo ""
-echo -e "\033[1;91m└──────────────────────────────────────────┘\033[0m"
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "        Autoscript Mod by ARTA MAULANA        "
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu-tr
