0f1e8791e35bcae28fcfb81c2f174748d6681eac
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-12-28 18:27:49 +0700

    Update cek-ws.sh

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
-blue='\033[0;34m'
-ungu='\033[0;35m'
-Green="\033[32m"
-Red="\033[31m"
-WhiteB="\e[5;37m"
-BlueCyan="\e[5;36m"
-Green_background="\033[42;37m"
-Red_background="\033[41;37m"
-Suffix="\033[0m"
+#Decrypted By YADDY D PHREAKER
+#!/bin/bash
 DF='\e[39m'
 Bold='\e[1m'
 Blink='\e[5m'
@@ -43,12 +32,10 @@ function con() {
     fi
 }
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '#vm#' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "              VMESS USER LOGIN            $NC"
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
-echo -e "   User"     "       Last Login"    "  Usage"   " Total IP"
-echo -e "\033[1;91m┌──────────────────────────────────────────┐\033[0m"
+data=( `cat /etc/xray/config.json | grep '###' | cut -d ' ' -f 2 | sort | uniq`);
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m           CEK VMESS ACCOUNT            \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
@@ -71,20 +58,26 @@ jum=$(cat /tmp/ipvmess.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
+#iplimit=$(cat /etc/kytxz/limit/vmess/ip/${akun})
 jum2=$(cat /tmp/ipvmess.txt | wc -l)
 byte=$(cat /etc/vmess/${akun})
 lim=$(con ${byte})
 wey=$(cat /etc/limit/vmess/${akun})
 gb=$(con ${wey})
 lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-printf "  %-13s %-7s %-8s %2s\n"   "${akun}" "$lastlogin"  " ${gb}/${lim}"   "$jum2";
+echo -e " \033[1;36m┌──────────────────────────────────────┐\033[0m"
+printf "  %-13s %-7s %-8s %2s\n"   "  USEENAME : ${akun}" | lolcat
+printf "  %-13s %-7s %-8s %2s\n" "  LOGIN    : $lastlogin" | lolcat 
+printf "  %-13s %-7s %-8s %2s\n" "  LIMIT GB : ${gb}/${lim}" | lolcat  
+#printf "  %-13s %-7s %-8s %2s\n" "  LIMIT IP : $jum2/$iplimit" | lolcat;
+echo -e " \033[1;36m└──────────────────────────────────────┘\033[0m"
 fi 
 rm -rf /tmp/ipvmess.txt
 done
 rm -rf /tmp/other.txt
 echo ""
-echo -e "\033[1;91m└──────────────────────────────────────────┘\033[0m"
-echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
-echo -e "        Autoscript Mod by ARTA MAULANA       "
-echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu-vmess
