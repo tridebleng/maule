d9a69743787f22a1c07b87bf040b038ec5eb1b8e
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-12-29 22:10:28 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -422,7 +422,7 @@ DATE=$(date +'%d %B %Y')
 datediff() {
     d1=$(date -d "$1" +%s)
     d2=$(date -d "$2" +%s)
-    echo -e " ${z}│${NC}${z}Expiry Script $blue=${NC} ${green}$exp ${NC}( ${r}$(( (d1 - d2) / 86400 )) ${NC}Days ) $NC"
+    echo -e " ${z}│${NC}${z}Expiry Script $blue=${NC} ( ${r}$(( (d1 - d2) / 86400 )) ${NC}Days ) $NC"
 }
 mai="datediff "$Exp" "$DATE""
 echo -e " ${z}╭══════════════════════════════════════════════════════════╮${NC}" | lolcat
