0d3b80d6d4a88a9332affb6b355c64e583d52a57
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-12-29 20:55:58 +0700

    Update menu-ssh.sh

--- menu/menu-ssh.sh
+++ menu/menu-ssh.sh
@@ -1,50 +1,82 @@
 #!/bin/bash
-Green="\e[92;1m"
-RED="\033[31m"
-YELLOW="\033[33m"
-BLUE="\033[36m"
-FONT="\033[0m"
-GREENBG="\033[42;37m"
-REDBG="\033[41;37m"
-OK="${Green}--->${FONT}"
-ERROR="${RED}[ERROR]${FONT}"
-GRAY="\e[1;30m"
-NC='\e[0m'
-red='\e[1;31m'
-green='\e[0;32m'
-DF='\e[39m'
-Bold='\e[1m'
-Blink='\e[5m'
-yell='\e[33m'
-red='\e[31m'
-green='\e[32m'
-blue='\e[34m'
-PURPLE='\e[35m'
-cyan='\e[36m'
-Lred='\e[91m'
-Lgreen='\e[92m'
-Lyellow='\e[93m'
-NC='\e[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-LIGHT='\033[0;37m'
-grenbo="\e[92;1m"
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-# Getting
-export CHATID="5795571992"
-export KEY="6351704520:AAEG3kwRGOUT4ta1e_q904v1zd-NtLFdXLE"
-export TIME="10"
-export URL="https://api.telegram.org/bot$KEY/sendMessage"
-clear
-#IZIN SCRIPT
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
 MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
-clear
+Name=$(curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini | grep $MYIP | awk '{print $3}')
+Isadmin=$(curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini | grep $MYIP | awk '{print $4}')
+fi
+
+
+export sem=$( curl -s https://raw.githubusercontent.com/tridebleng/ali/main/versions)
+export pak=$( cat /home/.ver)
+IPVPS=$(curl -s ipinfo.io/ip )
+#link izin ip vps
+ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 # Valid Script
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/tridebleng/permission/main/ipmini"
+data_ip="https://raw.githubusercontent.com/tridebleng/permisson/main/ipmini"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
@@ -63,12 +95,7 @@ checking_sc() {
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     exit
   fi
-}
-clear
-#Domain
-domain=$(cat /etc/xray/domain)
-
-if [ "$Isadmin" = "Pro" ]; then
+if [ "$Isadmin" = "ON" ]; then
 uis="${GREEN} ┌───────────────────────────────────────┐
    ꜱᴄʀɪᴘᴛ : PREMIUM         ᴠᴇʀꜱɪᴏɴ : $pak
    ᴜꜱᴇʀ   : $Name    ꜱᴛᴀᴛᴜꜱ
@@ -77,7 +104,7 @@ uis="${GREEN} ┌─────────────────────
 else
 uis="${RED} ┌───────────────────────────────────────┐
    ꜱᴄʀɪᴘᴛ : ꜰʀᴇᴇ         ᴠᴇʀꜱɪᴏɴ : $pak
-   ᴜꜱᴇʀ   : $Name            ꜱᴛᴀᴛᴜꜱ
+   ᴜꜱᴇʀ   : ꜰʀᴇᴇ            ꜱᴛᴀᴛᴜꜱ
    ᴇxᴘɪʀᴇ : ᴜɴʟɪᴍɪᴛᴇᴅ       $update
  └───────────────────────────────────────┘ $NC"
 fi
