313fe145d1a7a45ec487c236ebc57cc3e026786d
tridebleng <110109155+tridebleng@users.noreply.github.com>
2023-12-29 22:29:56 +0700

    Update menu-ssh.sh

--- menu/menu-ssh.sh
+++ menu/menu-ssh.sh
@@ -31,6 +31,7 @@ LIGHT='\033[0;37m'
 grenbo="\e[92;1m"
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 # Getting
+Name=$(curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini | grep $MYIP | awk '{print $2}')
 ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 export CHATID="5795571992"
 export KEY="6351704520:AAEG3kwRGOUT4ta1e_q904v1zd-NtLFdXLE"
