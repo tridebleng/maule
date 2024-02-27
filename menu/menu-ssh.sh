b748255e60079cc130e15baaaa0393c8ce54f542
tridebleng <110109155+tridebleng@users.noreply.github.com>
2024-02-28 06:24:41 +0700

    Update menu-ssh.sh

--- menu/menu-ssh.sh
+++ menu/menu-ssh.sh
@@ -31,10 +31,6 @@ LIGHT='\033[0;37m'
 grenbo="\e[92;1m"
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 # Getting
-Name=$(curl -sS https://raw.githubusercontent.com/tridebleng/permission/main/ipmini | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-ssh1="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 export CHATID="5795571992"
 export KEY="6351704520:AAEG3kwRGOUT4ta1e_q904v1zd-NtLFdXLE"
 export TIME="10"
@@ -62,55 +58,72 @@ checking_sc() {
     echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
     echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
     echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/Arta1ove"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6283162336146"
+    echo -e "      \033[0;36mTelegram${NC} t.me/arta"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/083162336146"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     exit
   fi
 }
+checking_sc
+echo -e "\e[32mloading...\e[0m"
 clear
 #Domain
 domain=$(cat /etc/xray/domain)
 
-if [ "$Isadmin" = "Pro" ]; then
-uis="${GREEN} ┌───────────────────────────────────────┐
-   ꜱᴄʀɪᴘᴛ : PREMIUM         ᴠᴇʀꜱɪᴏɴ : $pak
-   ᴜꜱᴇʀ   : $Name    ꜱᴛᴀᴛᴜꜱ
-   ᴇxᴘɪʀᴇ : $sisa_hari       $update
- └───────────────────────────────────────┘ $NC"
-else
-uis="${RED} ┌───────────────────────────────────────┐
-   ꜱᴄʀɪᴘᴛ : ꜰʀᴇᴇ         ᴠᴇʀꜱɪᴏɴ : $pak
-   ᴜꜱᴇʀ   : $Name            ꜱᴛᴀᴛᴜꜱ
-   ᴇxᴘɪʀᴇ : ᴜɴʟɪᴍɪᴛᴇᴅ       $update
- └───────────────────────────────────────┘ $NC"
-fi
-clear
-echo -e "$uis"
-#echo -e ""
-echo -e " \e[32m┌───────────────────────────────────────┐${NC}" | lolcat
-echo -e "            ═══[ ꜱꜱʜ ᴍᴇɴᴜ ]═══"                                       | lolcat
-echo -e " \e[32m└───────────────────────────────────────┘${NC}" | lolcat
-echo -e "   [1] ᴄʀᴇᴀᴛᴇ ᴀᴄᴄᴏᴜɴᴛ ꜱꜱʜ"
-echo -e "   [2] ᴛʀɪᴀʟ ᴀᴄᴄᴏᴜɴᴛ ꜱꜱʜ"
-echo -e "   [3] ᴇxᴛᴇɴᴅ ᴀᴄᴄᴏᴜɴᴛ ꜱꜱʜ"
-echo -e "   [4] ᴅᴇʟᴇᴛᴇ ᴀᴄᴄᴏᴜɴᴛ ꜱꜱʜ"
-echo -e "   [5] ᴄʜᴇᴄᴋ ᴜꜱᴇʀ ʟᴏɢɪɴ"
-echo -e "   [6] List Member SSH Account ${GREEN}$ssh1 $NC"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "                SSH & OpenVPN           "
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "  ${ORANGE}[1].${NC}\033[0;36m Create SSH & OpenVPN Account${NC}"
+echo -e "  ${ORANGE}[2].${NC}\033[0;36m Trial SSH & OpenVPN ${NC}"
+echo -e "  ${ORANGE}[3].${NC}\033[0;36m Renew SSH & OpenVPN ${NC}"
+echo -e "  ${ORANGE}[4].${NC}\033[0;36m Check User Login SSH & OpenVPN${NC}"
+echo -e "  ${ORANGE}[5].${NC}\033[0;36m Daftar Member SSH & OpenVPN ${NC}"
+echo -e "  ${ORANGE}[6].${NC}\033[0;36m Hapus SSH & OpenVpn Account ${NC}"
+echo -e "  ${ORANGE}[7].${NC}\033[0;36m Hapus User Expired SSH & OpenVPN ${NC}"
+echo -e "  ${ORANGE}[8].${NC}\033[0;36m Set up Autokill SSH ${NC}"
+echo -e "  ${ORANGE}[9].${NC}\033[0;36m Cek User Multi Login SSH ${NC}"
+echo -e " ${ORANGE}[10].${NC}\033[0;36m Lock Account User ${NC}"
+echo -e " ${ORANGE}[11].${NC}\033[0;36m Unlock Account User ${NC}"
+echo -e " ${ORANGE}[12].${NC}\033[0;36m Exit ${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+read -p "Select From Options [ 1 - 12 ] : " menu
 echo -e ""
-#echo -e " \e[32m┌───────────────────────────────────────┐${NC}" | lolcat
-echo -e "   [x]   ʙᴀᴄᴋ ᴛᴏ ᴍᴇɴᴜ"
-echo -e " \e[32m─────────────────────────────────────────${NC}" | lolcat
-echo -e ""
-read -p " Select menu :  "  opt
-echo -e ""
-case $opt in
-1) clear ; addssh ; exit ;;
-2) clear ; trial ; exit ;;
-3) clear ; hapus ; exit ;;
-4) clear ; renew ; exit ;;
-5) clear ; cek ; exit ;;
-6) clear ; member ; exit ;;
-x) menu ;;
-*) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
+case $menu in
+1)
+    usernew
+    ;;
+2)
+    trial
+    ;;
+3)
+    renew
+    ;;
+4)
+    cek
+    ;;
+5)
+    member
+    ;;
+6)
+    hapus
+    ;;
+7)
+    delete
+    ;;
+8)
+    autokill
+    ;;
+9)
+    ceklim
+    ;;
+10)
+    user-lock
+    ;;
+11)
+    user-unlock
+    ;;    
+*)
+    menu
+    ;;
 esac
